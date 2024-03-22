2607ef7ad0bed80e47be8150fa0b2beba2960c84
David <981402838@qq.com>
2024-03-22 09:16:33 +0800

    首次修改测试

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -7,13 +7,13 @@ export default defineUserConfig({
   locales: {
     "/": {
       lang: "en-US",
-      title: "Blog Demo",
-      description: "A blog demo for vuepress-theme-hope",
+      title: "David's Blog Demo",
+      description: "A blog demo besed on vuepress-theme-hope",
     },
     "/zh/": {
       lang: "zh-CN",
-      title: "博客演示",
-      description: "vuepress-theme-hope 的博客演示",
+      title: "我的博客演示",
+      description: "基于 vuepress-theme-hope 的博客演示",
     },
   },
 
